85d6fd48c8b6c55d8007a829ca2dc7b17b04125b
Avinash Kumar <ak2449746@gmail.com>
2022-05-08 03:29:55 +0530

    update...

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,6 +2,7 @@
 // Name, Email, Password, and Pic/ ProfilePic of the User
 
 const mongoose = require("mongoose")
+const bcrypt = require('bcryptjs')
 
 const userModel = mongoose.Schema(
     {
@@ -18,6 +19,19 @@ const userModel = mongoose.Schema(
         timestamps: true
     }
 )
+
+userModel.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+userModel.pre('save', async function (next) {
+    if (!this.isModified) {
+        next()
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
 
 const User = mongoose.model("User", userModel)
 
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const { chats } = require("./data/data")
 const connectDB = require("./config/db")
 const colors = require("colors")
 const userRoutes = require('./routes/userRoutes')
+const { notFound, errorHandler } = require('./middleware/errorMiddleware')
+
 
 dotenv.config()
 
@@ -28,6 +30,9 @@ app.get("/", (req, res) => {
 // })
 
 app.use('/api/user', userRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 5000
